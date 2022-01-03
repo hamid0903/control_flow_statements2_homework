ca3ef1c0a94921091547308abfca842a6d7731c6
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-03 20:26:48 +0500

    Ex08-1

--- if08.py
+++ if08.py
@@ -14,4 +14,21 @@ def main(number):
     Returns:
         str: return answer.
     """
-    return
+
+    if number==1:
+        i="Monday"
+    elif number==2:
+        i="Tuesday"
+    elif number==3:
+        i="Wednesday"
+    elif number==4:
+        i="Thursday"
+    elif number==5:
+        i="Friday"
+    elif number==6:
+        i="Saturday"
+    elif number==7:
+        i="Sunday"
+    return i
+x=main(1)
+print(x)
