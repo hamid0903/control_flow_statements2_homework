4d9cad06b3870d132864b1e7efbdbed9339ef129
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-03 20:34:22 +0500

    Ex05-2

--- if05.py
+++ if05.py
@@ -14,16 +14,16 @@ def main(n):
     n//=10
     n4=n%10
     n5=n//10
-    print(n1,n2,n3,n4,n5)
+    
     mx=n1
     if n2>mx:
         mx=n2
-    elif n3>mx:
+    if n3>mx:
         mx=n3
-    elif n4>mx:
+    if n4>mx:
         mx=n4
-    elif n5>mx:
+    if n5>mx:
         mx=n5
     return mx
-x=main(12345)
+x=main(59873)
 print(x)
