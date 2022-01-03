9181e73ff18e81d2bd575943975b44db1758215f
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-03 20:23:57 +0500

    Ex06-1

--- if06.py
+++ if06.py
@@ -6,4 +6,29 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    n1=n%10
+    n//=10
+    n2=n%10
+    n//=10
+    n3=n%10
+    n//=10
+    n4=n%10
+    n5=n//10
+    mx=n1
+    inx=1
+    if n2>mx:
+        mx=n2
+        inx=2
+    if n3>mx:
+        mx=n3
+        inx=3
+    if n4>mx:
+        mx=n4
+        inx=4
+    if n5>mx:
+        mx=n5
+        inx=5
+
+    return inx
+x=main(98765)
+print(x)
